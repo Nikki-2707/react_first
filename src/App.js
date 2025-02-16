00220f3b98c37d34cf5b566d4ef71a3c778e0d63
Nehas2707 <nehasharma.it22@scet.ac.in>
2025-02-16 19:57:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import React , {useState} from "react";
-// import About from "./Components/About";
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
 import Alert from "./Components/Alert";
-// import {
-//   BrowserRouter,
-//   Route,
-//   Routes
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
 
 
 function App() {
@@ -25,7 +25,16 @@ function App() {
     },1500)
   }
 
-  const toggleMode=()=>{
+  const removeBodyClasses=()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-primary')
+  }
+  const toggleMode=(cls)=>{
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls)
     if(mode ==='light') {
       setMode('dark');
       document.body.style.backgroundColor='rgb(28 44 69)';
@@ -41,23 +50,25 @@ function App() {
   }
   return (
     <>
-      {/* <BrowserRouter> */}
+      <BrowserRouter>
       <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       {/* <Navbar/> */}
       {/* <Navbar title="TextUtils"/> */}
       <div className="container my=5">
-      {/* <Routes>
+      <Routes>
         {/* /user --> Component 1
         /users/home --> Component 2 */}
-          {/* <Route exact path="/about" element={<About mode={mode} toggleMode={toggleMode}/>}/>
-      </Routes> */ }
-      {/* <Routes> */}
+          <Route exact path="/about" element={<About mode={mode} toggleMode={toggleMode}/>}/>
+      </Routes> 
+      <Routes>
+        <Route path="/" element={
           <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} toggleMode={toggleMode}/>
-      {/* </Routes> */}
+        }/>
+      </Routes>
       {/* <About/> */}
       </div>
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </>
   );
 }
